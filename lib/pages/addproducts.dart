c3ed253bee61144c584ac5e794226ee6ef748eec
minniee <minsalaehrad@gmail.com>
2022-03-07 20:33:23 +0700

    first commit

---
+++ lib/pages/addproducts.dart
@@ -0,0 +1,195 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebasedemo/pages/showproduct.dart';
+import 'package:flutter/material.dart';
+
+class AddProductPage extends StatefulWidget {
+  const AddProductPage({Key? key}) : super(key: key);
+
+  @override
+  _AddProductPageState createState() => _AddProductPageState();
+}
+
+class _AddProductPageState extends State<AddProductPage> {
+  TextEditingController _name = TextEditingController();
+  TextEditingController _price = TextEditingController();
+  TextEditingController _type = TextEditingController();
+
+  CollectionReference products =
+      FirebaseFirestore.instance.collection('Products');
+  Future<void> addProduct() {
+    return products
+        .add({
+          'product_name': _name.text,
+          'price': _price.text,
+          'product_type': _type.text,
+        })
+        .then((value) => print("Product data has been successfully"))
+        .catchError((error) => print("Failed to add data: $error"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('image/2.jpg'), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Stack(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 35, top: 30),
+              child: const Text(
+                'เพิ่มข้อมูลสินค้า',
+                style: TextStyle(color: Colors.cyan, fontSize: 33),
+              ),
+            ),
+            formfield(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SingleChildScrollView formfield(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.only(top: 170),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(left: 35, right: 35),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _name,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      label: Text(
+                        'Product name',
+                        style: TextStyle(
+                            fontSize: 20, color: Colors.grey.shade700),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    controller: _price,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      label: Text(
+                        'Price',
+                        style: TextStyle(
+                            fontSize: 20, color: Colors.grey.shade700),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    controller: _type,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      label: Text(
+                        'Product Type',
+                        style: TextStyle(
+                            fontSize: 20, color: Colors.grey.shade700),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'บันทึกข้อมูล',
+                        style: TextStyle(
+                            color: Colors.cyan,
+                            fontSize: 27,
+                            fontWeight: FontWeight.w700),
+                      ),
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Color.fromARGB(255, 238, 204, 231),
+                        child: IconButton(
+                            color: Colors.pink,
+                            onPressed: () {
+                              addProduct();
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const ShowProductPage(),
+                                ),
+                              ).then((value) => setState(() {}));
+                            },
+                            icon: const Icon(
+                              Icons.arrow_forward,
+                            )),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/showproduct.dart
@@ -0,0 +1,143 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebasedemo/pages/addproducts.dart';
+import 'package:flutter/material.dart';
+
+class ShowProductPage extends StatefulWidget {
+  const ShowProductPage({Key? key}) : super(key: key);
+
+  @override
+  _ShowProductPageState createState() => _ShowProductPageState();
+}
+
+class _ShowProductPageState extends State<ShowProductPage> {
+  CollectionReference products =
+      FirebaseFirestore.instance.collection('Products');
+
+  Future<void> deleteProduct({required String id}) {
+    return products
+        .doc(id)
+        .delete()
+        .then((value) => print("Deleted data Successfully"))
+        .catchError((error) => print("Failed to delete user: $error"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('image/2.jpg'), fit: BoxFit.cover),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: ListView(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(left: 100),
+              child: Text(
+                'สินค้าทั้งหมด',
+                style: TextStyle(color: Colors.cyan, fontSize: 33),
+              ),
+            ),
+            showlist(),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const AddProductPage(),
+              ),
+            ).then((value) => setState(() {}));
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+
+  Widget showlist() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('Products').snapshots(),
+      // builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+      builder: (context, snapshot) {
+        List<Widget> listMe = [];
+        if (snapshot.hasData) {
+          var products = snapshot.data;
+          listMe = [
+            Column(
+              children: products!.docs.map((DocumentSnapshot doc) {
+                Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+                return Card(
+                  child: ListTile(
+                    onTap: () {},
+                    title: Text(
+                      '${data['product_name']}',
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                          color: Color.fromARGB(255, 236, 114, 155)),
+                    ),
+                    subtitle: Text('${data['price']}' + ' THB'),
+                    trailing: IconButton(
+                      onPressed: () {
+                        var alertDialog = AlertDialog(
+                          title: const Text('ยืนยันการลบข้อมูลสินค้า'),
+                          content: Text(
+                              'คุณต้องการลบสินค้า ${data['product_name']} ใช่หรือไม่'),
+                          actions: [
+                            TextButton(
+                                onPressed: () => Navigator.pop(context),
+                                child: const Text('ยกเลิก')),
+                            TextButton(
+                                onPressed: () {
+                                  deleteProduct(id: doc.id)
+                                      .then((value) => Navigator.pop(context));
+                                },
+                                child: const Text(
+                                  'ยืนยัน',
+                                  style: TextStyle(color: Colors.red),
+                                )),
+                          ],
+                        );
+                        showDialog(
+                          context: context,
+                          builder: (context) => alertDialog,
+                        );
+                      },
+                      icon: const Icon(
+                        Icons.delete_forever,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          ];
+        }
+        return Center(
+          child: Column(
+            children: listMe,
+          ),
+        );
+      },
+    );
+  }
+
+  Container txt() {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 20),
+      alignment: Alignment.center,
+      child: const Text(
+        'Products list',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
